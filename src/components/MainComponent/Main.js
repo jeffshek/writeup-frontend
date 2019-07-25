f7317409dde8676f830252dfe263a13276529452
Jeffrey Shek <jeffshek@gmail.com>
2019-07-24 23:58:04 -0500

    Begin Mocking Around Lorem Ipsum

--- src/components/MainComponent/Main.js
+++ src/components/MainComponent/Main.js
@@ -1,13 +1,12 @@
-import React, {Fragment} from "react";
-import {MainStyles} from "components/MainComponent/Main.styles";
+import React, { Fragment } from "react";
+import { MainStyles } from "components/MainComponent/Main.styles";
 import withStyles from "@material-ui/core/styles/withStyles";
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import Paper from "@material-ui/core/Paper/Paper";
 import Grid from "@material-ui/core/Grid/Grid";
 import Typography from "@material-ui/core/Typography/Typography";
 import Button from "@material-ui/core/Button/Button";
-import {TopbarComponent} from "components/TopbarComponent/Topbar";
-
+import { TopbarComponent } from "components/TopbarComponent/Topbar";
 
 export class _MainComponent extends React.Component {
   render() {
@@ -16,91 +15,81 @@ export class _MainComponent extends React.Component {
     return (
       <Fragment>
         <TopbarComponent />
-      <div className={classes.root}>
-        <Grid container justify="center">
-          <Grid spacing={4} alignItems="center" justify="center" container className={classes.grid}>
-            <Grid item xs={12} md={4}>
-              <Paper className={classes.paper}>
-                <div className={classes.box}>
-                  <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
-                    First title
-                  </Typography>
-                  <Typography variant="body2" gutterBottom>
-                    A first title style <br/> with two lines
-                  </Typography>
-                </div>
-                <div style={{display: 'flex', justifyContent: 'flex-end'}}>
-                  <Button color='primary' variant="contained" className={classes.actionButtom}>
-                    Learn more
-                  </Button>
-                </div>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <Paper className={classes.paper}>
-                <div className={classes.box}>
-                  <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
-                    Another box
-                  </Typography>
-                  <Typography variant="body1" gutterBottom>
-                    A default box
-                  </Typography>
-                </div>
-                <div style={{display: 'flex', justifyContent: 'flex-end'}}>
-                  <Button color='primary' variant="contained" className={classes.actionButtom}>
-                    Learn more
-                  </Button>
-                </div>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <Paper className={classes.paper}>
-                <div className={classes.box}>
-                  <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
-                    A box with a carousel
-                  </Typography>
-                  <Typography variant="body1" gutterBottom>
-                    If you click in Getting Started, you will see a nice carousel
-                  </Typography>
-                </div>
-                <div className={classes.alignRight}>
-                  <Button onClick={this.openDialog}  variant="outlined" className={classes.actionButtom}>
-                    Learn more
-                  </Button>
-                  <Button onClick={this.openGetStartedDialog} color='primary' variant="contained" className={classes.actionButtom}>
-                    Dashboard
-                  </Button>
-                </div>
-              </Paper>
-            </Grid>
-            <Grid container item xs={12}>
-              <Grid item xs={12}>
-                <Paper className={classes.paper}>
-                  <div>
-                    <div className={classes.box}>
-                      <Typography color='secondary' gutterBottom>
-                        Full box
-                      </Typography>
-                      <Typography variant="body1" gutterBottom>
-                        This is an example of a full-width box
-                      </Typography>
+        <div className={classes.root}>
+          <Grid container justify="center">
+            <Grid
+              spacing={4}
+              alignItems="center"
+              justify="center"
+              container
+              className={classes.grid}
+            >
+              <Grid container item xs={12}>
+                <Grid item xs={12}>
+                  <Paper className={classes.paper}>
+                    <div>
+                      <div className={classes.box}>
+                        <Typography color="secondary" gutterBottom>
+                          Writing Prompt ...
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Integer eu sem vel nisi scelerisque finibus
+                          vitae a nibh. Maecenas sed sem felis. Fusce feugiat,
+                          mauris nec tempus varius, nisl diam porttitor turpis,
+                          non pretium ipsum leo id urna. Pellentesque mattis
+                          lectus ornare justo hendrerit, sit amet blandit odio
+                          semper. Suspendisse ut dapibus sapien. Phasellus
+                          mollis at metus eget imperdiet. Proin lobortis, neque
+                          ac mattis hendrerit, dolor justo elementum lectus, eu
+                          molestie risus lorem vel felis. Proin a lacinia quam.
+                          Praesent laoreet tristique turpis, vel lobortis lectus
+                          tempus quis. Etiam efficitur, velit ac bibendum
+                          rutrum, lacus odio mollis erat, in sollicitudin massa
+                          lorem sed lorem. In bibendum diam nec ligula
+                          consectetur iaculis. Nunc posuere mollis risus, id
+                          efficitur neque feugiat in. Proin eu augue in turpis
+                          convallis cursus.
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Integer eu sem vel nisi scelerisque finibus
+                          vitae a nibh. Maecenas sed sem felis. Fusce feugiat,
+                          mauris nec tempus varius, nisl diam porttitor turpis,
+                          non pretium ipsum leo id urna. Pellentesque mattis
+                          lectus ornare justo hendrerit, sit amet blandit odio
+                          semper. Suspendisse ut dapibus sapien. Phasellus
+                          mollis at metus eget imperdiet. Proin lobortis, neque
+                          ac mattis hendrerit, dolor justo elementum lectus, eu
+                          molestie risus lorem vel felis. Proin a lacinia quam.
+                          Praesent laoreet tristique turpis, vel lobortis lectus
+                          tempus quis. Etiam efficitur, velit ac bibendum
+                          rutrum, lacus odio mollis erat, in sollicitudin massa
+                          lorem sed lorem. In bibendum diam nec ligula
+                          consectetur iaculis. Nunc posuere mollis risus, id
+                          efficitur neque feugiat in. Proin eu augue in turpis
+                          convallis cursus.
+                        </Typography>
+                      </div>
+                      <div className={classes.alignRight}>
+                        <Button
+                          color="primary"
+                          variant="contained"
+                          className={classes.actionButtom}
+                        >
+                          Learn more
+                        </Button>
+                      </div>
                     </div>
-                    <div className={classes.alignRight}>
-                      <Button color='primary' variant="contained" className={classes.actionButtom}>
-                        Learn more
-                      </Button>
-                    </div>
-                  </div>
-                </Paper>
+                  </Paper>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </div>
-      </Fragment>);
+        </div>
+      </Fragment>
+    );
   }
 }
 
-
 export const MainComponent = withRouter(withStyles(MainStyles)(_MainComponent));
-
--- src/components/MainComponent/Main.styles.js
+++ src/components/MainComponent/Main.styles.js
@@ -1,80 +1,78 @@
-import backgroundShape from "images/shape.svg"
-
+import backgroundShape from "images/shape.svg";
 
 export const MainStyles = theme => ({
   root: {
     flexGrow: 1,
-    backgroundColor: theme.palette.grey['100'],
-    overflow: 'hidden',
+    backgroundColor: theme.palette.grey["100"],
+    overflow: "hidden",
     background: `url(${backgroundShape}) no-repeat`,
-    backgroundSize: 'cover',
-    backgroundPosition: '0 400px',
+    backgroundSize: "cover",
+    backgroundPosition: "0 400px",
     paddingBottom: 200
   },
   grid: {
     width: 1200,
     marginTop: 40,
-    [theme.breakpoints.down('sm')]: {
-      width: 'calc(100% - 20px)'
+    [theme.breakpoints.down("sm")]: {
+      width: "calc(100% - 20px)"
     }
   },
   paper: {
     padding: theme.spacing(3),
-    textAlign: 'left',
-    color: theme.palette.text.secondary,
+    textAlign: "left",
+    color: theme.palette.text.secondary
   },
   rangeLabel: {
-    display: 'flex',
-    justifyContent: 'space-between',
+    display: "flex",
+    justifyContent: "space-between",
     paddingTop: theme.spacing(2)
   },
   topBar: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginTop: 32
   },
   outlinedButtom: {
-    textTransform: 'uppercase',
+    textTransform: "uppercase",
     margin: theme.spacing(1)
   },
   actionButtom: {
-    textTransform: 'uppercase',
+    textTransform: "uppercase",
     margin: theme.spacing(1),
     width: 152
   },
   blockCenter: {
     padding: theme.spacing(2),
-    textAlign: 'center'
+    textAlign: "center"
   },
   block: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(2)
   },
   box: {
-    marginBottom: 40,
-    height: 65
+    marginBottom: 60,
+    height: 200
   },
   inlining: {
-    display: 'inline-block',
+    display: "inline-block",
     marginRight: 10
   },
   buttonBar: {
-    display: 'flex'
+    display: "flex"
   },
   alignRight: {
-    display: 'flex',
-    justifyContent: 'flex-end'
+    display: "flex",
+    justifyContent: "flex-end"
   },
   noBorder: {
-    borderBottomStyle: 'hidden'
+    borderBottomStyle: "hidden"
   },
   loadingState: {
     opacity: 0.05
   },
   loadingMessage: {
-    position: 'absolute',
-    top: '40%',
-    left: '40%'
+    position: "absolute",
+    top: "40%",
+    left: "40%"
   }
 });
-
