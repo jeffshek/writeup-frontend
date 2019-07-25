fd51ed8c1bfc0f18a69fe29c0c07f8b53363c62d
Jeffrey Shek <jeffshek@gmail.com>
2019-07-24 23:31:58 -0500

    settle on tagline (or at least for the next hour)

--- src/components/TopbarComponent/Topbar.js
+++ src/components/TopbarComponent/Topbar.js
@@ -1,116 +1,135 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid/Grid";
 import Typography from "@material-ui/core/Typography/Typography";
-import {Link, withRouter} from "react-router-dom";
-import {TopbarStyles} from "components/TopbarComponent/Topbar.styles";
-import withStyles from '@material-ui/styles/withStyles';
+import { Link, withRouter } from "react-router-dom";
+import { TopbarStyles } from "components/TopbarComponent/Topbar.styles";
+import withStyles from "@material-ui/styles/withStyles";
 
-import Toolbar from '@material-ui/core/Toolbar';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import MenuIcon from '@material-ui/icons/Menu';
-import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import {Link as MaterialLink} from '@material-ui/core'
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import logo from "images/logo.svg"
-import {Menu} from "components/Menu"
-import {TopbarMenu} from "components/TopbarMenu";
+import Toolbar from "@material-ui/core/Toolbar";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import MenuIcon from "@material-ui/icons/Menu";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import { Link as MaterialLink } from "@material-ui/core";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import logo from "images/logo.svg";
+import { Menu } from "components/Menu";
+import { TopbarMenu } from "components/TopbarMenu";
 
-
-const InlineTagline = ({classes}) => {
+const InlineTagline = ({ classes }) => {
   return (
     <div className={classes.inline}>
-    <Typography variant="h6" color="inherit" noWrap>
-      <Link to='/' className={classes.link}>
-        <img width={20} src={logo} alt="writeup.ai logo"/>
-        <span className={classes.tagline}>writeup.ai</span>
-      </Link>
-    </Typography>
-  </div>)
-}
-
-const TextTagline = ({classes}) => {
-  return (
-    <div className={classes.productLogo}>
-      <Typography>
-        write, right. fast.
+      <Typography variant="h6" color="inherit" noWrap>
+        <Link to="/" className={classes.link}>
+          <img width={20} src={logo} alt="writeup.ai logo" />
+          <span className={classes.tagline}>writeup.ai</span>
+        </Link>
       </Typography>
     </div>
-  )
-}
+  );
+};
 
+const TextTagline = ({ classes }) => {
+  return (
+    <div className={classes.productLogo}>
+      <Typography className={classes.strikeInline}>right</Typography>
+      <Typography className={classes.inlineBlockTagLine}>
+        write, fast.
+      </Typography>
+    </div>
+  );
+};
 
 class _TopbarComponent extends Component {
   state = {
-    value:      0,
+    value: 0,
     menuDrawer: false
   };
 
   handleChange = (event, value) => {
-    this.setState({value});
+    this.setState({ value });
   };
 
-  mobileMenuOpen = (event) => {
-    this.setState({menuDrawer: true});
-  }
+  mobileMenuOpen = event => {
+    this.setState({ menuDrawer: true });
+  };
 
-  mobileMenuClose = (event) => {
-    this.setState({menuDrawer: false});
-  }
+  mobileMenuClose = event => {
+    this.setState({ menuDrawer: false });
+  };
 
   componentDidMount() {
     window.scrollTo(0, 0);
   }
 
   current = () => {
-    if (this.props.currentPath === '/home') {
-      return 0
+    if (this.props.currentPath === "/home") {
+      return 0;
     }
-    if (this.props.currentPath === '/dashboard') {
-      return 1
+    if (this.props.currentPath === "/dashboard") {
+      return 1;
     }
-    if (this.props.currentPath === '/signup') {
-      return 2
+    if (this.props.currentPath === "/signup") {
+      return 2;
     }
-    if (this.props.currentPath === '/wizard') {
-      return 3
+    if (this.props.currentPath === "/wizard") {
+      return 3;
     }
-    if (this.props.currentPath === '/cards') {
-      return 4
+    if (this.props.currentPath === "/cards") {
+      return 4;
     }
-
-  }
+  };
 
   renderMobileIconContainer = () => {
-    const {classes} = this.props;
+    const { classes } = this.props;
 
     return (
       <div className={classes.iconContainer}>
-        <IconButton onClick={this.mobileMenuOpen} className={classes.iconButton} color="inherit" aria-label="Menu">
-          <MenuIcon/>
+        <IconButton
+          onClick={this.mobileMenuOpen}
+          className={classes.iconButton}
+          color="inherit"
+          aria-label="Menu"
+        >
+          <MenuIcon />
         </IconButton>
       </div>
-    )
-  }
+    );
+  };
 
   renderTabContainer = () => {
-    const {classes} = this.props;
+    const { classes } = this.props;
 
     return (
       <div className={classes.tabContainer}>
-        <SwipeableDrawer anchor="right" open={this.state.menuDrawer} onClose={this.mobileMenuClose}
-                         onOpen={this.mobileMenuOpen}>
-          <AppBar title="Menu"/>
+        <SwipeableDrawer
+          anchor="right"
+          open={this.state.menuDrawer}
+          onClose={this.mobileMenuClose}
+          onOpen={this.mobileMenuOpen}
+        >
+          <AppBar title="Menu" />
           <List>
             {Menu.map((item, index) => (
-              <ListItem component={item.external ? MaterialLink : Link} href={item.external ? item.pathname : null}
-                        to={item.external ? null : {pathname: item.pathname, search: this.props.location.search}} button
-                        key={item.label}>
-                <ListItemText primary={item.label}/>
+              <ListItem
+                component={item.external ? MaterialLink : Link}
+                href={item.external ? item.pathname : null}
+                to={
+                  item.external
+                    ? null
+                    : {
+                        pathname: item.pathname,
+                        search: this.props.location.search
+                      }
+                }
+                button
+                key={item.label}
+              >
+                <ListItemText primary={item.label} />
               </ListItem>
             ))}
           </List>
@@ -123,35 +142,44 @@ class _TopbarComponent extends Component {
           onChange={this.handleChange}
         >
           {Menu.map((item, index) => (
-            <Tab key={index} component={item.external ? MaterialLink : Link} href={item.external ? item.pathname : null}
-                 to={item.external ? null : {pathname: item.pathname, search: this.props.location.search}}
-                 classes={{root: classes.tabItem}} label={item.label}/>
+            <Tab
+              key={index}
+              component={item.external ? MaterialLink : Link}
+              href={item.external ? item.pathname : null}
+              to={
+                item.external
+                  ? null
+                  : {
+                      pathname: item.pathname,
+                      search: this.props.location.search
+                    }
+              }
+              classes={{ root: classes.tabItem }}
+              label={item.label}
+            />
           ))}
         </Tabs>
       </div>
-    )
-  }
+    );
+  };
 
   render() {
-
-    const {classes} = this.props;
+    const { classes } = this.props;
 
     return (
       <AppBar position="absolute" color="default" className={classes.appBar}>
         <Toolbar>
           <Grid container spacing={2} alignItems="baseline">
-
             <Grid item xs={10} className={classes.flex}>
-              <InlineTagline classes={classes}/>
+              <InlineTagline classes={classes} />
               {!this.props.noTabs && (
                 <React.Fragment>
-                  {/*<TextTagline classes={classes}/>*/}
-                  {/*{this.renderMobileIconContainer()}*/}
+                  <TextTagline classes={classes} />
+                  {this.renderMobileIconContainer()}
                   {this.renderTabContainer()}
                 </React.Fragment>
               )}
             </Grid>
-
             <Grid item xs={2} className={classes.flex}>
               <div className={classes.tabContainerRight}>
                 <Tabs
@@ -161,22 +189,32 @@ class _TopbarComponent extends Component {
                   onChange={this.handleChange}
                 >
                   {TopbarMenu.map((item, index) => (
-                    <Tab key={index} component={item.external ? MaterialLink : Link}
-                         href={item.external ? item.pathname : null}
-                         to={item.external ? null : {pathname: item.pathname, search: this.props.location.search}}
-                         classes={{root: classes.tabItem}} label={item.label}/>
+                    <Tab
+                      key={index}
+                      component={item.external ? MaterialLink : Link}
+                      href={item.external ? item.pathname : null}
+                      to={
+                        item.external
+                          ? null
+                          : {
+                              pathname: item.pathname,
+                              search: this.props.location.search
+                            }
+                      }
+                      classes={{ root: classes.tabItem }}
+                      label={item.label}
+                    />
                   ))}
                 </Tabs>
               </div>
             </Grid>
-
           </Grid>
         </Toolbar>
       </AppBar>
-    )
+    );
   }
 }
 
-
-export const TopbarComponent = withRouter(withStyles(TopbarStyles)(_TopbarComponent));
-
+export const TopbarComponent = withRouter(
+  withStyles(TopbarStyles)(_TopbarComponent)
+);
--- src/components/TopbarComponent/Topbar.styles.js
+++ src/components/TopbarComponent/Topbar.styles.js
@@ -1,73 +1,80 @@
 export const TopbarStyles = theme => ({
   appBar: {
-    position: 'relative',
-    boxShadow: 'none',
-    borderBottom: `1px solid ${theme.palette.grey['100']}`,
-    backgroundColor: 'white',
-
+    position: "relative",
+    boxShadow: "none",
+    borderBottom: `1px solid ${theme.palette.grey["100"]}`,
+    backgroundColor: "white"
   },
   inline: {
-    display: 'inline'
+    display: "inline"
   },
   flex: {
-    display: 'flex',
-    [theme.breakpoints.down('sm')]: {
-      display: 'flex',
-      justifyContent: 'space-evenly',
-      alignItems: 'center'
+    display: "flex",
+    [theme.breakpoints.down("sm")]: {
+      display: "flex",
+      justifyContent: "space-evenly",
+      alignItems: "center"
     }
   },
   link: {
-    textDecoration: 'none',
-    color: 'inherit'
+    textDecoration: "none",
+    color: "inherit"
   },
   productLogo: {
-    display: 'inline-block',
-    borderLeft: `1px solid ${theme.palette.grey['A100']}`,
+    display: "inline-block",
+    borderLeft: `1px solid ${theme.palette.grey["A100"]}`,
     marginLeft: 32,
     paddingLeft: 24,
-    [theme.breakpoints.up('md')]: {
-      paddingTop: '1.5em'
+    [theme.breakpoints.up("md")]: {
+      paddingTop: "1.5em"
     }
   },
   tagline: {
-    display: 'inline-block',
+    display: "inline-block",
     fontWeight: "normal",
     marginLeft: 10,
-    [theme.breakpoints.up('md')]: {
-      paddingTop: '0.8em'
+    [theme.breakpoints.up("md")]: {
+      paddingTop: "0.8em"
     }
   },
   iconContainer: {
-    display: 'none',
-    [theme.breakpoints.down('sm')]: {
-      display: 'block'
+    display: "none",
+    [theme.breakpoints.down("sm")]: {
+      display: "block"
     }
   },
   iconButton: {
-    float: 'right'
+    float: "right"
   },
   tabContainer: {
     marginLeft: 32,
-    [theme.breakpoints.down('sm')]: {
-      display: 'none'
+    [theme.breakpoints.down("sm")]: {
+      display: "none"
     }
   },
   tabContainerRight: {
     marginLeft: 32,
-    [theme.breakpoints.down('sm')]: {
-                  display: 'none'
-                },
-    [theme.breakpoints.up('md')]: {
-      paddingTop: '0.8em'
+    [theme.breakpoints.down("sm")]: {
+      display: "none"
+    },
+    [theme.breakpoints.up("md")]: {
+      paddingTop: "0.8em"
     }
   },
   tabItem: {
     paddingTop: 20,
     paddingBottom: 20,
-    minWidth: 'auto'
+    minWidth: "auto"
   },
   settingsContainer: {
     paddingBottom: "0.5rem"
+  },
+  strikeInline: {
+    display: "inline-block",
+    paddingRight: "0.25rem",
+    "text-decoration": "line-through"
+  },
+  inlineBlockTagLine: {
+    display: "inline-block"
   }
-})
+});
