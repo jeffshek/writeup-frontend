0d3acdf88cd84fbde85303bf85e3f58149b4fb55
Jeffrey Shek <jeffshek@gmail.com>
2019-07-24 23:39:10 -0500

    Hide middle section, since I don't know what you're going to do with it yet

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,28 +1,27 @@
 export const Menu = [
-  {
-    label: "Home",
-    pathname: "/"
-  },
-  {
-    label: "Dashboard",
-    pathname: "/dashboard"
-  },
-  {
-    label: "Signup",
-    pathname: "/signup"
-  },
-  {
-    label: "Wizard",
-    pathname: "/wizard"
-  },
-  {
-    label: "Cards",
-    pathname: "/cards"
-  },
-  {
-    label: "Github",
-    pathname: "https://github.com/alexanmtz/material-sense",
-    external: true
-  }
-
+  //{
+  //  label: "Home",
+  //  pathname: "/"
+  //},
+  //{
+  //  label: "Dashboard",
+  //  pathname: "/dashboard"
+  //},
+  //{
+  //  label: "Signup",
+  //  pathname: "/signup"
+  //},
+  //{
+  //  label: "Wizard",
+  //  pathname: "/wizard"
+  //},
+  //{
+  //  label: "Cards",
+  //  pathname: "/cards"
+  //},
+  //{
+  //  label: "Github",
+  //  pathname: "https://github.com/alexanmtz/material-sense",
+  //  external: true
+  //}
 ];
