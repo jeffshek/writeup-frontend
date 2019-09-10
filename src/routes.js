9afb32332bf7d74516ee911df7b95e49f946d4f5
Jeffrey Shek <jeffshek@gmail.com>
2019-09-10 09:15:19 -0400

    Add Legal URLs to Hotmap

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import { PrivacyPolicyComponent } from "components/PrivacyPolicy";
 export default props => (
   <BrowserRouter>
     <Route exact path="/" component={MainComponent} />
+    <Route exact path="/legal" component={MainComponent} />
     <Route exact path="/prompts/:uuid/" component={PublishedPromptComponent} />
     <Route exact path="/best/" component={BestPromptsComponent} />
     <Route exact path="/policy/" component={PrivacyPolicyComponent} />
